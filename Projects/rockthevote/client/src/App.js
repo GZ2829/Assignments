c4a6ae662ff74a7c6524a348187b6a88d5e65f2d
GZ2829 <gzucchero24@gmail.com>
2018-06-14 17:34:41 -0600

    rock the vote

--- Projects/rockthevote/client/src/App.js
+++ Projects/rockthevote/client/src/App.js
@@ -2,36 +2,61 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux'
 import { getData } from './redux'
 import './App.css';
+import Issue from './Issue'
 
 class App extends Component {
     constructor(){
       super()
 
-      this.state ={
+      this.state = {
+        title: '',
+        description: '',
+        isToggled: false
+    }
 
-      }
+    this.handleChange = this.handleChange.bind(this)
+    this.handleSubmit = this.handleSubmit.bind(this)
+    this.toggle = this.toggle.bind(this)
+
     }
 
     componentDidMount(){
        this.props.getData()
   }
+  handleChange(e){
+    const { name, value } = e.target
+    this.setState({
+        [name]: value
+    })
+}
+
+handleSubmit(e){
+    e.preventDefault()
+    let newTodo = {
+            title: this.state.title,
+            description: this.state.description
+        }
+    this.props.editTodo( this.props._id, newTodo )
+    this.setState({ isToggled: false, title: '', description: '' })
+}
+
+toggle(){
+    this.setState(prevState => {
+        return {
+            isToggled: !prevState.isToggled
+        }
+    })
+}
+
+
     
   render() {
     const iss = this.props.issues.map(issue=>
-        <div>
-          <h1>{issue.title}</h1>
-          <h3>{issue.description}</h3>
-          <p>{issue.counter}</p>
-          <button>+</button>
-          <button>-</button>
-          <button>Comment</button>
-          <button>edit</button>
-          <button>delete</button>
-        </div>
+       <Issue title={issue.title} description={issue.description} counter={issue.counter}/>
     )
     return (
       <div>
-       {iss}
+       {iss}    
       </div>
     );
   }
---
+++ Projects/rockthevote/client/src/Issue.js
@@ -0,0 +1,82 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux'
+import './App.css';
+
+class Issue extends Component {
+    constructor(){
+      super()
+
+      this.state = {
+        title: '',
+        description: '',
+        isToggled: false
+    }
+
+    this.handleChange = this.handleChange.bind(this)
+    this.handleSubmit = this.handleSubmit.bind(this)
+    this.toggle = this.toggle.bind(this)
+
+    }
+
+  handleChange(e){
+    const { name, value } = e.target
+    this.setState({
+        [name]: value
+    })
+}
+
+handleSubmit(e){
+    e.preventDefault()
+    let newTodo = {
+            title: this.state.title,
+            description: this.state.description
+        }
+    this.props.editTodo( this.props._id, newTodo )
+    this.setState({ isToggled: false, title: '', description: '' })
+}
+
+toggle(){
+    this.setState(prevState => {
+        return {
+            isToggled: !prevState.isToggled
+        }
+    })
+}
+
+
+    
+  render() {
+    return (
+      <div>
+        <h1>{this.props.title}</h1>
+        <h3>{this.props.description}</h3>
+        <p>{this.props.counter}</p>
+        <button>+</button>
+        <button>-</button>
+        <button onClick={this.toggle}>Comment</button>
+        <button>edit</button>
+        <button>delete</button>
+        {this.state.isToggled 
+            ? <form onSubmit={ this.handleSubmit }>
+                <input 
+                    type="text"     
+                    value={this.state.title} 
+                    name="title" 
+                    onChange={ this.handleChange }
+                    placeholder="Title"/>
+                <input 
+                    type="text" 
+                    value={this.state.description} 
+                    name="description" 
+                    onChange={ this.handleChange }
+                    placeholder="Description"/>
+                    <button>Submit</button>
+                </form>
+              : null
+              }  
+      </div>
+    );
+  }
+}
+
+export default connect(state=>({ issues: state }))(Issue)
